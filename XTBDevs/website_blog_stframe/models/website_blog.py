c7075527e3e1d298537b09d7046348ec2a30a25c
ulisesgamboa <ulises.gamboa@xmartb.com>
2022-02-09 18:48:17 -0600

    update module blog

--- XTBDevs/website_blog_stframe/models/website_blog.py
+++ XTBDevs/website_blog_stframe/models/website_blog.py
@@ -6,11 +6,11 @@ from odoo import api, models, fields, _
 class BlogPost(models.Model):
     _inherit = "blog.post"
 
-    iframe_stream = fields.Text('iFrame Completo src', help='Solo el src del iframe')
+    iframe_stream = fields.Text('iFrame Completo src', help="Solo el src del iframe")
     iframe_stream_height = fields.Integer('Altura (px)', default=200)
-    iframe_stream_mini = fields.Text('iFrame Tarjeta src', help='Solo el src del iframe mini')
+    iframe_stream_mini = fields.Text('iFrame Tarjeta src', help="Solo el src del iframe mini")
     iframe_stream_mini_height = fields.Integer('Altura (px)', default=50)
-    iframe_stream_side = fields.Text('iFrame Lateral src', help='Solo el src del iframe lateral')
+    iframe_stream_side = fields.Text('iFrame Lateral src', help="Solo el src del iframe lateral")
     iframe_stream_side_height = fields.Integer('Altura (px)', default=50)
     download_stream = fields.Text('Podcast Download link')
 
