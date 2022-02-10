92427829449e5c44f27637a8cd0be29fe77a6354
ulisesgamboa <ulises.gamboa@xmartb.com>
2022-02-09 18:20:44 -0600

    WebSite blog stream

---
+++ XTBDevs/website_blog_stframe/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "iFrame Stream",
+    "version": "15.0",
+     # Author
+    'author': 'TECHNOLOGY & XMART BUSINESS',
+    'website': 'https://www.xmartb.com',
+    'maintainer': 'XMARTB',
+    "depends": [
+        'website',
+        'website_blog',
+    ],
+    "demo": [
+    ],
+    "data": [
+        'views/website_blog_templates.xml',
+        'views/website_blog_views.xml',
+    ],
+    "auto_install": False,
+    "installable": True,
+}
